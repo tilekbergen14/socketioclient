af7174570f584878da835e31623d40c93f14d433
TQ <tilekbergen.third@gmail.com>
2024-05-13 11:49:46 -0700

    changes

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import AppLayout from '@/layout/AppLayout.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: () => import('@/views/Homeview.vue'),
+        },
+        {
+            path: '/landing',
+            name: 'landing',
+            component: () => import('@/views/pages/Landing.vue')
+        },
+        {
+            path: '/pages/notfound',
+            name: 'notfound',
+            component: () => import('@/views/pages/NotFound.vue')
+        },
+
+        {
+            path: '/auth/login',
+            name: 'login',
+            component: () => import('@/views/pages/auth/Login.vue')
+        },
+        {
+            path: '/auth/access',
+            name: 'accessDenied',
+            component: () => import('@/views/pages/auth/Access.vue')
+        },
+        {
+            path: '/auth/error',
+            name: 'error',
+            component: () => import('@/views/pages/auth/Error.vue')
+        }
+    ]
+});
+
+export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+</script>
+
+<template>
+    <div class="flex flex-column m-4 align-center text-center">
+        <h2>Simple chat application with Socket.IO</h2>
+        <div><InputText v-model="msg" placeholder="Room..." class="flex-1" @keyup.enter="sendMsg()" /> <Button label="Join" class="ml-2" @click="sendMsg()" /></div>
+    </div>
+</template>
